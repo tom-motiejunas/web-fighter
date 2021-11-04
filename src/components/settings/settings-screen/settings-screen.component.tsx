f26855a28dd593e54f6fcc5fc05a9648df098c45
tom <tom.motiejunas@gmail.com>
2021-11-04 12:11:22 +0200

    fixed settings bug

--- src/components/settings/settings-screen/settings-screen.component.tsx
+++ src/components/settings/settings-screen/settings-screen.component.tsx
@@ -34,6 +34,8 @@ function SettingsScreen({ setShowSettings, setPlayerHp, setBotHp }: Props) {
       } else {
         if (!settings) return;
         if (e.target.dataset.setting === "maxHp") {
+          if (!e.target.value) return;
+          console.log(e.target.value);
           setBotHp(+e.target.value);
           setPlayerHp(+e.target.value);
         }
